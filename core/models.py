886744ee99f40674a2e71fa067193513b2452fb7
diegos <diego_suriaga@trendmicro.com>
2022-09-12 23:08:39 +0800

    first commit

---
+++ core/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+import uuid
+# Create your models here.
+
+class CustomUserManager():
+    def create_user(self, email, first_name, last_name, password, **kw):
+        if not email:
+            raise ValueError('The given email address must be set')
+        if not password:
+            raise ValueError('Password is not provided')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            first_name = first_name,
+            last_name = last_name,
+            **kw
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **kw):
+
+        user = self._create_user(email, password, **kw)
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    uuid = models.UUIDField(default = uuid.uuid4, editable = False)
+    email = models.EmailField(db_index=True, unique=True, max_length=254)
+    first_name = models.CharField(max_length=240)
+    middle_name =  models.CharField(max_length=120)
+    last_name =  models.CharField(max_length=120)
+    password = models.CharField(max_length=120)
+    employee_id = models.CharField(max_length=120)
+    joined_on = models.DateTimeField(blank=True, null=True)
+
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
