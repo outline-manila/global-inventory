a9f9e6beafb1d81595e6fc2ac42b46fe8026f8ed
suriagadiego <diego.suriaga@expedock.com>
2023-02-04 01:32:32 +0800

    test

--- global_backend/settings.py
+++ global_backend/settings.py
@@ -86,7 +86,6 @@ WSGI_APPLICATION = 'global_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE'),
